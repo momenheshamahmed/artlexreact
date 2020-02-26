91a9b30ea2f3ab6da728bfc06524442043a45d3d
Momen Hesham <momenheshamzaza@gmail.com>
2020-02-26 21:44:20 +0200

    Typeface Page
    Typeface Page Components

--- src/components/TypefacePageComponents/BuyTypefaceSection/BuyTypefaceComponent/index.tsx
+++ src/components/TypefacePageComponents/BuyTypefaceSection/BuyTypefaceComponent/index.tsx
@@ -6,9 +6,19 @@ import { useObserver } from "mobx-react";
 import styled from "styled-components";
 import { Container, Row, Col } from "react-bootstrap";
 import { Typography, Button } from "@material-ui/core";
-import AddIcon from '@material-ui/icons/Add';
+import AddIcon from "@material-ui/icons/Add";
 
 const BuyTypefaceComponent: React.FC = () => {
+  const BuyFontContainer = styled(Container)`
+    background: #F7F7F7;
+    padding: 2rem;
+  `;
+  const TextAndButton = styled(Row)`
+  align-items: center;
+  `
+  const WeightPreview = styled(Col)`
+  height: 100%;
+  `
   const CustomButton = styled(Button)`
     background: #00ff00 !important;
     border-radius: 100px !important;
@@ -18,28 +28,32 @@ const BuyTypefaceComponent: React.FC = () => {
     }
   `;
   return useObserver(() => (
-    <>
-      <Container>
-        <Row>
-          <Typography variant="subtitle1" component="subtitle1" className="mb-3 mt-3">
+    <BuyFontContainer>
+      {/* <Row>
+        <Col>
+          <Typography variant="subtitle1" component="subtitle1">
             PAIR FONTS
           </Typography>
-        </Row>
-        <Row>
-          <Col>Name</Col>
-          <Col className="text-right align-middle">
-            <CustomButton
-              variant="contained"
-              color="primary"
-              className="mt-3"
-              endIcon={<AddIcon />}
-            >
-              Add to Cart
-            </CustomButton>
-          </Col>
-        </Row>
-      </Container>
-    </>
+        </Col>
+      </Row> */}
+      <TextAndButton>
+        <WeightPreview>
+          <Typography variant="subtitle1" component="subtitle1">
+            PAIR FONTS
+          </Typography>
+        </WeightPreview>
+        <Col className="text-right align-middle">
+          <CustomButton
+            variant="contained"
+            color="primary"
+            
+            endIcon={<AddIcon />}
+          >
+            Add to Cart
+          </CustomButton>
+        </Col>
+      </TextAndButton>
+    </BuyFontContainer>
   ));
 };
 export default BuyTypefaceComponent;
